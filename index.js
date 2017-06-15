a154886ec1f136fae7d834133200833eb1e923c7
Chris Jamieson <jamiesoncj@gmail.com>
2017-06-15 17:25:48 +0100

    Add some tests and split out functions into files

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 'use strict';
 
 var debug = require('debug')('express-idempotency');
-var md5 = require('md5');
-var LRU = require('lru-cache');
 var connect = require('connect');
 var expressEnd = require('express-end');
 
-var CACHE_MAX_AGE = 2 * 24 * 60 * 60 * 1000; // 48 hours
-
-var lruOptions = {
-  max: 500,
-  length: function (n, key) { return n * 2 + key.length },
-  maxAge: CACHE_MAX_AGE
-};
-
-var cache = LRU(lruOptions);
-
-/**
- * Generate cache key for idempotent request
- * @param {object} req Express request object
- * @param {string} idempotencyKey Idempotency key
- * @return {string}
- */
-function generateCacheKey(req, idempotencyKey) {
-  const path = req.path;
-  const body = JSON.stringify(req.body);
-  const md5hash = md5(`${path}+${body}`);
-  const cacheKey = `idem_${md5hash}_${idempotencyKey}`;
-  return cacheKey;
-}
+var cache = require('./lib/cache-provider');
+var generateCacheKey = require('./lib/generate-cache-key');
 
 /**
  * Express middleware
@@ -70,7 +47,9 @@ function storeMw(req, res, next) {
       const responseToStore = {
         statusCode: res.statusCode,
         body: res.body,
+        headers: res.headers,
       };
+
       const cacheKey = generateCacheKey(req, idempotencyKey);
       cache.set(cacheKey, responseToStore)
       debug('stored response against idempotency key: ', idempotencyKey);
@@ -82,9 +61,9 @@ function storeMw(req, res, next) {
 var idempotency = function (options) {
   // chain pattern from helmet - see https://github.com/helmetjs/helmet/blob/master/index.js
   var chain = connect();
-  chain.use(expressEnd());
-  chain.use(checkMw());
-  chain.use(storeMw());
+  chain.use(expressEnd);
+  chain.use(checkMw);
+  chain.use(storeMw);
   return chain;
 }
 
--- test/test.js
+++ test/test.js
@@ -1,11 +1,169 @@
 'use strict';
 
 var expect = require('chai').expect;
+var httpMocks = require('node-mocks-http');
+var expressEnd = require('express-end');
+var util = require('util');
 
-// TODO write some tests
-// see http://sinonjs.org/ and https://stackoverflow.com/questions/34516951/express-middleware-testing-mocha-chai for MW ideas
-describe('# Request hash generator', function() {
-  it('should generate a cache key', function() {
-    generateCacheKey();
+var middleware = require('../index.js')();
+var generateCacheKey = require('../lib/generate-cache-key');
+var cache = require('../lib/cache-provider');
+
+describe('# Express Idempotency', function() {  
+  describe('request handler creation', function() {
+    var mw;
+
+    beforeEach(function() {
+      mw = middleware;
+    });
+
+    it('should return a function()', function() {
+      expect(mw).to.be.a('Function');
+    });
+
+    it('should accept three arguments', function() {
+      expect(mw.length).to.equal(3);
+    });
+  });
+
+  describe('middleware', function() {
+    var req, res;
+    context('with idempotency-key header in request', function() {
+      before(function(done) {
+        req = httpMocks.createRequest({
+          method: 'POST',
+          url: '/test/path',
+          headers: {
+            'Idempotency-Key': 'just-a-dummy-key-1',
+          },
+        });
+        
+        res = httpMocks.createResponse({
+          eventEmitter: require('events').EventEmitter
+        });
+        
+        done();
+      });
+
+      it('stores a response to the cache', function(done) {
+        middleware(req, res, function next(error) {
+          if (error) {
+            throw new Error('Expected not to receive an error');
+          }
+
+          // end the res
+          res.once('end', function() {
+            var idempotencyKey = req.get('Idempotency-Key');
+            var cacheKey = generateCacheKey(req, idempotencyKey);
+            var storedResponse = cache.get(cacheKey);
+            if (!storedResponse) {
+              throw new Error('Response was not stored in cache');
+            }
+
+            expect(storedResponse).to.have.property('statusCode');
+            expect(storedResponse).to.have.property('body');
+            expect(storedResponse).to.have.property('headers');
+
+            done();
+          });
+
+          // set a dummy response status and body
+          res.statusCode = 200;
+          res.body = { id: 1234, description: 'Just a dummy!' };
+          res.send();
+        });
+      });
+
+      context('with stored response in the cache', function() {
+        var responseToStore = {
+          statusCode: 403,
+          body: { exampleProp: 'hey there!' },
+          headers: { 'x-dummy-header': 'hip hop hoop' },
+        };
+
+        before(function(done) {
+          var idempotencyKey = 'just-a-dummy-key-2';
+          req = httpMocks.createRequest({
+            method: 'POST',
+            url: '/test/path',
+            headers: {
+              'Idempotency-Key': idempotencyKey,
+            },
+          });
+          
+          res = httpMocks.createResponse({
+            eventEmitter: require('events').EventEmitter
+          });
+
+          const cacheKey = generateCacheKey(req, idempotencyKey);
+          cache.set(cacheKey, responseToStore);
+          done();
+        });
+
+        it('returns a stored response from the cache', function(done) {
+          res.once('end', function() {
+            var idempotencyKey = req.get('Idempotency-Key');
+            var cacheKey = generateCacheKey(req, idempotencyKey);
+            var storedResponse = cache.get(cacheKey);
+            if (!storedResponse) {
+              throw new Error('Response was not stored in cache');
+            }
+
+            var expectedHeaders = responseToStore.headers;
+            expectedHeaders['X-Cache'] = 'HIT'; // expect a cache header
+
+            expect(res._getStatusCode()).to.equal(responseToStore.statusCode);
+            expect(res._getData()).to.deep.equal(responseToStore.body);
+            expect(res._getHeaders()).to.deep.equal(expectedHeaders);
+
+            done();
+          });
+
+          middleware(req, res, function next(error) {
+            if (error) {
+              console.log('err: ', error);
+              throw new Error('Expected not to receive an error');
+            }
+          });
+        });
+      });
+    });
+
+    context('without idempotency-key header in request', function() {
+      beforeEach(function(done) {
+        req = httpMocks.createRequest({
+          method: 'POST',
+          url: '/test/path',
+          headers: {},
+        });
+        res = httpMocks.createResponse({
+          eventEmitter: require('events').EventEmitter
+        });
+        
+        done();
+      });
+
+      it('does not store a response to the cache', function(done) {
+        res.once('end', function() {
+          var idempotencyKey = req.get('Idempotency-Key');
+          var cacheKey = generateCacheKey(req, idempotencyKey);
+          var storedResponse = cache.get(cacheKey);
+          if (storedResponse) {
+            throw new Error('A response was erroneously stored in cache');
+          }
+          done();
+        });
+
+        middleware(req, res, function next(error) {
+          if (error) {
+            throw new Error('Expected not to receive an error');
+          }
+
+          // set a dummy response status and body
+          res.status(200);
+          res.send({ id: 1234, description: 'Just a dummy!' });
+        });
+      });
+    });
   });
 });
